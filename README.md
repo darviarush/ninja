766745182a2ba636cc773beb47a70fdf5f3a2652
darviarush <halfdroy@gmail.com>
2021-06-17 13:43:36 +0300

    master save

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ninja
-Редактор (IDE) в стиле Smalltalk, но для разных языков программирования
+# NAME
+
+ninja - редактор (IDE) в стиле Smalltalk, но для разных языков программирования.
+
+# VERSION
+
+0.0.0-prealpha
+
+# DEPENDENS
+
+* perl-tk
+
+# INSTALL
+
+```sh
+$ git clone https://github.com/darviarush/ninja.git
+$ cd ninja
+$ perl ninja.pl
+```
+
+# AUTHOR
+
+Yaroslav O. Kosmina <darviarush@mail.ru>
--- ninja.pl
+++ ninja.pl
@@ -15,29 +15,27 @@ $root->Button(
 my $main = $root->Panedwindow(qw/-orient vertical/);
 my $sections = $root->Panedwindow(qw/-orient horizontal/);
 
-my $class_categories = $root->Listbox;
-$class_categories->pack(-side => 'left');
-$sections->add($class_categories);
+sub sec {
+	my $f1 = $root->Frame;
+	my $list = $f1->Listbox;
+	$list->pack(qw/-fill both -expand 1/);
+	my $entry = $f1->Entry;
+	$entry->pack(-side => 'bottom');
+	$sections->add($f1);
+	return $list, $entry;
+}
 
-my $classes = $root->Listbox;
-$classes->pack(-side => 'left');
-$sections->add($classes);
+my ($rank, $rank_filter) = sec();
+my ($classes, $class_filter) = sec();
+my ($categories, $category_filter) = sec();
+my ($methods, $method_filter) = sec();
 
-my $method_categories = $root->Listbox;
-$method_categories->pack(-side => 'left');
-$sections->add($method_categories);
-
-my $methods = $root->Listbox;
-$methods->pack(-side => 'left');
-$sections->add($methods);
-
-$sections->pack(-side => 'top');
-$main->add($sections);
 
 my $text = $root->Text;
 $text->pack(-side => 'top');
-$main->add($text);
+$sections->pack(-side => 'top');
 
+$main->add($sections, $text);
 $main->pack(-fill=>'both', -expand=>1);
 
 MainLoop;
